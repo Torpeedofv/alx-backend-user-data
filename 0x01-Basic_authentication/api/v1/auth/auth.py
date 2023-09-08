5d70c816151014bdff0612d88d3e056520f8ca05
Torpeedofv <torpeedo.fv@gmail.com>
2023-09-08 08:15:57 +0100

    task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,16 +3,17 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
+    """auth class"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False"""
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """returns None"""
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None"""
