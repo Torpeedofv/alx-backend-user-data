20bfff00333b9db093227a5af2d34e1a6ae7d263
Torpeedofv <torpeedo.fv@gmail.com>
2023-09-13 17:50:25 +0100

    task 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,6 +7,7 @@ Base = declarative_base()
 
 
 class User(Base):
+    """An SQLAlchemy model clas"""
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
