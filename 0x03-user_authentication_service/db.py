4cc4cdf8b517299b36f0f09480bc043679f93baa
Torpeedofv <torpeedo.fv@gmail.com>
2023-09-14 10:44:56 +0100

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,10 +41,7 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """returns the first row found in users table"""
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound("No user Found")
-            return user
-        except InvalidRequestError as e:
-            raise e
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound()
+        return user
